97d21c40f2a20bf8bff5d5ec0b3fa62513564ded
Chris Grimmett <chris@grimtech.net>
2016-02-24 11:17:53 -0500

    OLD progress

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ firstwin
 ========
 
 Tweets you when your league of legends summoner's first win of the day is available
+
+
+
+
+
+
+### dev notes
+
+#### What determines an unelligable match
+
+* Intro Mode: Bots only buy:
+  * Doran's blade (id: )
+  * Doran's shield (id: )
+  * Boots of speed (id: )
+  * Doran's ring (id: )
+
+  
--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var http = require('http');
 var app = express();
 var server = http.createServer(app);
 var nconf = require('nconf');
-var legends = require('legends');
-//var league_api = require('league-api');
+var league = require('gnar');
+var redis = require('redis');
 
 
 nconf.env(['PORT', 'RIOTKEY'])
@@ -17,27 +17,102 @@ nconf.defaults({
 console.log('key: ' + nconf.get('RIOTKEY'));
 port = nconf.get('PORT');
 
-//var league = new league_api('RIOTKEY');
-var league = legends(nconf.get('RIOTKEY'), 'na');
+var gnar = league(nconf.get('RIOTKEY'), 'na');
+var red = redis.createClient();
 
-app.get('/', function(req, res) {
-    res.send('root of roots');
+
+//
+// FUNCTIONS
+//
+
+// que requests
+// send them here
+var queReq = function queReq(req) {
+    
 });
 
+// find a new player
+var getNoob = function getNoob() {
+    red.get("constant/noobSearchId", function(err, id) {
+	if (err) throw err;
+	if (!id) id = 59000000;
 
-app.get('/firstwin/:summoner', function(req, res) {
-    var summoner = req.params.summoner;
-
-    league.getSummonerByName(summoner, function(err, data) {
-	if (err) return console.error('error getting name: ' + err);
-
-	    league.getRankedStats(data.id, function(err, stats) {
-	    if (err) return console.error('error getting stats: ' + err);
-	    console.log(stats);
-
-	    res.send('summoner stats: ' + stats);	    
-	});
+	
+	return gnar.summoner.by_id(id).pipe(process.stdout);
     });
+};
+
+
+
+
+//
+// ROUTES
+//
+app.get('/', function(req, res) {
+    res.send('root of roots');
+    getNoob().pipe
+});
+
+app.get('/firstwin/:name', function(req, res) {
+    // get summoner id
+    // get match history using summoner id
+    // get last win using match history
+    // 24 hours from last win = firstwin available
+    
+    var name = req.params.name.replace(/\s/g, "").toLowerCase();
+    console.log('name is ' + name);
+    var summoner = gnar.summoner.by_name(name); //.pipe(process.stdout);
+
+    var bod = '';
+    summoner.on('data', function(chunk) {
+	console.log('got %d B of data', chunk.length);
+	bod += chunk;
+    });
+
+    summoner.on('end', function() {
+	var summonerId = JSON.parse(bod)[name].id;
+
+	var game = gnar.game.by_summoner(summonerId);
+
+	var g = '';
+	game.on('data', function(chunk) {
+	    console.log('got some %dB of game data ', chunk.length);
+	    g += chunk;
+	});
+
+	game.on('end', function() {
+	    console.log('end of game');
+
+	    var lastGame = JSON.parse(g).games[0];
+	    //console.dir(g);
+
+	    //console.log('ending game, here is the game: ' );
+	    console.log('most recent game win status: ' + lastGame.stats.win);
+
+	    // * get last 10 most recent games
+	    // * get normal games
+	    //   * get the winning games
+	    //   * average = average ipEarned
+	    // 
+	    
+	    //console.dir(lastGame.stats.win);
+	    return res.send(g);
+	});
+	//return res.send(bod);
+    });
+
+		
+    //console.log('summoner id is: ' + summoner.id);
+    //console.dir(summoner);
+//    var game = gnar.game.by_summoner(summoner[1].id).pipe(res);
+
+//    
+//    res.send(summoner);
+//    var history = gnar.matchhistory(summoner.id).pipe(res);
+
+    
+//    res.send(history);
+    
 });
 
 server.listen(port);
